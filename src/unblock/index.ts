89fedd9026f678a1a5746b3968004e91e7e4050f
niangkuo <30277962+kllk2@users.noreply.github.com>
2026-02-18 02:25:02 +0800

    Update index.ts

--- src/unblock/index.ts
+++ src/unblock/index.ts
@@ -1,1 +1,90 @@
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { SongUrlResult } from "./unblock";
+import axios from "axios";
+import getKuwoSongUrl from "./kuwo";
+import getBodianSongUrl from "./bodian";
+import getGequbaoSongUrl from "./gequbao";
 
+/**
+ * 直接获取 网易云云盘 链接
+ * Thank @939163156
+ * Power by GD音乐台(music.gdstudio.xyz)
+ */
+const getNeteaseSongUrl = async (id: number | string): Promise<SongUrlResult> => {
+  try {
+    if (!id) return { code: 404, url: null };
+    const baseUrl = "https://music-api.gdstudio.xyz/api.php";
+    const result = await axios.get(baseUrl, {
+      params: { types: "url", id },
+    });
+    const songUrl = result.data.url;
+    return { code: 200, url: songUrl };
+  } catch (error) {
+    return { code: 404, url: null };
+  }
+};
+
+// 初始化 UnblockAPI
+export const initUnblockAPI = async (fastify: FastifyInstance) => {
+  // 主信息
+  fastify.get("/unblock", (_, reply) => {
+    reply.send({
+      name: "UnblockAPI",
+      description: "SPlayer UnblockAPI service",
+      author: "@imsyy",
+      content:
+        "部分接口采用 @939163156 by GD音乐台(music.gdstudio.xyz)，仅供本人学习使用，不可传播下载内容，不可用于商业用途。",
+    });
+  });
+  // netease
+  fastify.get(
+    "/unblock/netease",
+    async (
+      req: FastifyRequest<{ Querystring: { [key: string]: string } }>,
+      reply: FastifyReply,
+    ) => {
+      const { id } = req.query;
+      const result = await getNeteaseSongUrl(id);
+      return reply.send(result);
+    },
+  );
+  // kuwo
+  fastify.get(
+    "/unblock/kuwo",
+    async (
+      req: FastifyRequest<{ Querystring: { [key: string]: string } }>,
+      reply: FastifyReply,
+    ) => {
+      const { keyword } = req.query;
+      const result = await getKuwoSongUrl(keyword);
+      return reply.send(result);
+    },
+  );
+  // bodian
+  fastify.get(
+    "/unblock/bodian",
+    async (
+      req: FastifyRequest<{ Querystring: { [key: string]: string } }>,
+      reply: FastifyReply,
+    ) => {
+      console.log("kllk fastify.get /unblock/bodian 被调用")
+      const { keyword } = req.query;
+      const result = await getBodianSongUrl(keyword);
+      console.log("kllk fastify.get /unblock/bodian 结果:", result)
+      return reply.send(result);
+    },
+  );
+  // gequbao
+  fastify.get(
+    "/unblock/gequbao",
+    async (
+      req: FastifyRequest<{ Querystring: { [key: string]: string } }>,
+      reply: FastifyReply,
+    ) => {
+      const { keyword } = req.query;
+      const result = await getGequbaoSongUrl(keyword);
+      return reply.send(result);
+    },
+  );
+
+};
