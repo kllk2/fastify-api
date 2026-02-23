9a9f107a35c81e817f9211805d938fd0f3d34ae1
niangkuo <30277962+kllk2@users.noreply.github.com>
2026-02-23 19:55:44 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
-import { initUnblockAPI } from '../src/unblock';
+import { initUnblockAPI } from '../src/unblock/index';
 
 const app = fastify({ logger: true });
 
